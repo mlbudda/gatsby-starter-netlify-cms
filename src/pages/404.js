b48b677090ab7947c082fa85327a9e9e5db4ed58
Ignas Markauskas <ignasmark5@gmail.com>
2021-04-14 01:20:22 +0300

    small change

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,6 +6,7 @@ const NotFoundPage = () => (
     <div>
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>Small change</p>
     </div>
   </Layout>
 )
